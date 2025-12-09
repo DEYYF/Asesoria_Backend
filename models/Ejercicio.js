ec7ceccd15c5dc3425edc55f9e196a25d2082fc9
UnaiProsolid <91943767+DEYYF@users.noreply.github.com>
2025-12-09 18:27:35 +0100

    Update Ejercicio.js

--- models/Ejercicio.js
+++ models/Ejercicio.js
@@ -14,14 +14,7 @@ const ejercicioSchema = new mongoose.Schema({
   instrucciones: String,
 }, { timestamps: true });
 
-// 👇 añade la función aquí mismo
-function normalize(str = "") {
-  return str
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .trim()
-    .toLowerCase();
-}
+
 
 module.exports = mongoose.model("Ejercicio", ejercicioSchema);
 
@@ -34,12 +27,7 @@ ejercicioSchema.index(
 );
 
 // Middleware para normalizar el nombre antes de guardar
-ejercicioSchema.pre('save', function(next) {
-  if (this.isModified('nombre')) {
-    this.nombreNormalized = normalize(this.nombre);
-  }
-  next();
-});
+
 
 const Ejercicio = mongoose.model('Ejercicio', ejercicioSchema);
 module.exports = Ejercicio;
